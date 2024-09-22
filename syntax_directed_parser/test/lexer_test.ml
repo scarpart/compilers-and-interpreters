8c5fb98754d26878e0c496ec8484f7a32d6f0c29
scarpart <aenartzr@gmail.com>
2024-09-22 20:42:38 -0300

    feat: fixing tests

--- syntax_directed_parser/test/lexer_test.ml
+++ syntax_directed_parser/test/lexer_test.ml
@@ -1,25 +1,25 @@
-open Alcotest;;
 open Syntax_directed_parser.Token;;
 open Syntax_directed_parser.Lexer;;
 open Core;;
-open Core_unix;;
 
 let create_in_channel_of_source s = 
-  In_channel.create ~binary:false s 
+  let cwd = Core_unix.getcwd () in 
+  In_channel.create ~binary:false (cwd ^ "/testfiles/" ^ s)
 
 let test_scan_input () = 
-  let input = create_in_channel_of_source "for ( var = 32; var < 400; var = var + 32 )" in 
+  let input = create_in_channel_of_source "forloop" in 
   let t1 = create_token_lexeme Id "for" in 
   let t2 = scan_token input in 
   Alcotest.(check token_testable) "same token" t1 t2
 
 let test_scan_digit () = 
-  let input = create_in_channel_of_source "43243 + 3217893131" in 
+  let input = create_in_channel_of_source "digits" in 
   let t1 = create_token_value Digit 43243 in 
   let t2 = scan_token input in 
   Alcotest.(check token_testable) "same token" t1 t2
 
 let () =
+  Printf.printf "this is a test\n%!";
   Alcotest.run "Lexer"
     [
       ( "lexer-scanning",
